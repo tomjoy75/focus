b4a37e1fccf7838fa4f0fe7c081e7e1aff1b7c9a
tomjoy75 <tomjoy75@gmail.com>
2026-01-26 11:54:28 +0100

    Refactor focus start
    focus start V1 done

--- src/focus.py
+++ src/focus.py
@@ -20,14 +20,15 @@ def get_data_dir():
         return 1
     return path
 
-def init_data(path, today_str) -> {} :
+def init_data(path, today_str) -> dict | None :
+    # Initialise data
     if path.is_file():
         with open(path, "r") as f:
             try :
                 data = json.loads(f.read())
             except (ValueError, json.JSONDecodeError, UnicodeDecodeError) :
                 print(f"⚠️ Invalid session file format.\nPlease fix or delete the file:\t{path}")
-                return
+                return None
     else :
         data = {
             "date": today_str,
@@ -36,10 +37,45 @@ def init_data(path, today_str) -> {} :
         print("Initialized empty daily state")
     return data
     
+def check_data(data) -> list | None:
+    # Check if data is a list and end is present
+    sessions = data["sessions"]
+    if not isinstance(sessions, list):
+        print("⚠️ Invalid session file format.")
+        return None
+    if len(sessions) != 0:
+        last = sessions[-1]
+        if "end" not in last:
+            print("⚠️ A focus session is already running.")
+            print("Use `focus status` to check it.")
+            return None
+    return sessions
+
+def prompt_user() -> str:
+    while True:
+        print("🎯 What are you going to do now?")
+        intent = input()
+        if intent.strip() != "":
+            break
+        else:
+            print("Your answer is empty, please enter a valid answer.")
+    return intent
+
+
+def start_session(intent, duration, path, data):
+    time = datetime.datetime.now()
+    start_time = time.strftime("%H:%M")
+    new_session = {
+        "intent" : intent,
+        "start" : start_time,
+        "planned_min_duration" : duration
+    }
+    data["sessions"].append(new_session)
+    with open(path, "w", encoding="utf-8") as f:
+        f.write(json.dumps(data, indent=4, ensure_ascii=False))
 
 def cmd_start():
-    # Se refaire un schema en pseudo code a avoir sous la main
-    # Diviser la fonction pour ne pas etre trop machine a gaz
+    # Start a new session
     data_dir = get_data_dir()
     time = datetime.datetime.now()
     today_str = time.strftime("%Y-%m-%d")
@@ -48,40 +84,20 @@ def cmd_start():
     print(f"Path of the file     : {path}")
 
     data = init_data(path, today_str)
-
-    sessions = data["sessions"]
-    if not isinstance(sessions, list):
-        print("⚠️ Invalid session file format.")
+    if data is None:
         return 1
-    if len(sessions) != 0:
-        last = sessions[-1]
-        if "end" not in last:
-            print("⚠️ A focus session is already running.")
-            print("Use `focus status` to check it.")
-            return(1)
 
-    while True:
-        print("🎯 What are you going to do now?")
-        intent = input()
-        if intent.strip() != "":
-            break
-        else:
-            print("Your answer is empty, please enter a valid answer.")
+    sessions = check_data(data)
+    if sessions is None:
+        return 1
 
-    time = datetime.datetime.now()
-    start_time = time.strftime("%H:%M")
-    new_session = {
-        "intent" : intent,
-        "start" : start_time,
-        "planned_min_duration" : 25
-    }
-    sessions.append(new_session)
-    with open(path, "w", encoding="utf-8") as f:
-        f.write(json.dumps(data, indent=4, ensure_ascii=False))
+    intent = prompt_user()
+    if intent is None:
+        return 1
+
+    start_session(intent, 25, path, data)
+
     return 0
-
-
-
 
 def cmd_status():
     print("Checking focus session WIP")
@@ -99,14 +115,16 @@ def main():
     cmd = sys.argv[1]
     match(cmd):
         case "start":
-            cmd_start()
+            return cmd_start()
         case "status":
             cmd_status()
         case "help" | "-h" :
             cmd_help()
+            return 0
         case _ :
             print(f"unknown command : {cmd}")
             cmd_help()
+            return 1
 
     #print("focus CLI - work in progress")
 
